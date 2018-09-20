b0a79edd36769bbc16e265e6f1f567eb8bdc26dc
moican <5764135+moican@users.noreply.github.com>
2018-09-20 18:07:15 +0200

    Add files via upload

---
+++ two-fer/TwoFer.fs
@@ -0,0 +1,10 @@
+﻿module TwoFer
+
+let twoFer (input: string option): string = 
+    let who = match input with
+               | Some i -> i
+               | _ -> "you" 
+    who |> sprintf "One for %s, one for me."
+
+
+
