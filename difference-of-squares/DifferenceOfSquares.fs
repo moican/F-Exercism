27ef076f39718507e61682d4fca6fc3f991c9e1c
moican <5764135+moican@users.noreply.github.com>
2018-09-20 17:45:31 +0200

    Add files via upload

---
+++ difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,9 @@
+﻿module DifferenceOfSquares
+
+let squareOfSum (number: int): int = 
+    let sum = Seq.sumBy (fun x -> x) [1..number]
+    sum * sum
+
+let sumOfSquares (number: int): int = Seq.sumBy (fun x -> x * x) [1..number]
+
+let differenceOfSquares (number: int): int = squareOfSum number - sumOfSquares number
