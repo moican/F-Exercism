e98bff394e77989841809c95ce30a0f2859a9173
moican <5764135+moican@users.noreply.github.com>
2018-09-20 17:33:02 +0200

    Add files via upload

---
+++ sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,8 @@
+﻿module SumOfMultiples
+
+let rec isMultipleOfAny (numbers: int list) (number: int) : bool = 
+    match numbers with
+        | head :: tail -> number % head = 0 || isMultipleOfAny tail number
+        | _ -> false
+let sum (numbers: int list) (upperBound: int): int = 
+    Seq.fold (fun acc x -> if isMultipleOfAny numbers x then x + acc else acc) 0 [1 .. upperBound - 1]
