9c6ce492b7e2a585e4af2157bb213d55da87598f
moican <5764135+moican@users.noreply.github.com>
2018-10-03 08:57:47 +0200

    Add files via upload

---
+++ accumulate/Accumulate.fs
@@ -0,0 +1,8 @@
+﻿module Accumulate
+
+let accumulate<'a, 'b> (func: 'a -> 'b) (input: 'a list): 'b list = 
+    let rec accumulateImp func acc = function
+        |head :: tail -> accumulateImp func (acc @ [func head]) tail
+        |[] -> acc
+
+    accumulateImp func [] input
