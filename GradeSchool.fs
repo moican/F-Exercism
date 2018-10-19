fcfe97e0cf8b8136d3919510f4db2242a18728cb
moican <5764135+moican@users.noreply.github.com>
2018-10-19 11:03:51 +0200

    Add files via upload

---
+++ GradeSchool.fs
@@ -0,0 +1,20 @@
+﻿module GradeSchool
+
+open System
+
+type School = Map<int, string list>
+
+let empty: School = Map.empty
+
+let add (student: string) (grade: int) (school: School): School =
+    school.Add(grade, 
+        List.sort(
+            match school.TryFind grade with
+            | Some v -> v @ [student] 
+            | None -> [student]))
+   
+let roster (school: School): (int * string list) list = 
+    school |> Seq.map (|KeyValue|)|> List.ofSeq
+       
+let grade (number: int) (school: School): string list = 
+    defaultArg (Map.tryFind number school) []
