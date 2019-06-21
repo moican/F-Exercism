f39fbd98698521ecc3f72b9c229b955812e435dd
moican <5764135+moican@users.noreply.github.com>
2019-06-21 08:24:42 +0200

    Add files via upload

---
+++ diffie-hellman/DiffieHellman.fs
@@ -0,0 +1,36 @@
+﻿module DiffieHellman
+
+open System.Numerics;
+open System.Security.Cryptography;
+
+let generator = RNGCryptoServiceProvider.Create()
+
+let getRandom (primeP :BigInteger) = 
+    let value = Array.zeroCreate(primeP.GetByteCount(true))
+    generator.GetBytes value
+    let possitiveNumber = value.[0] &&& (byte)0X7F
+    Array.set value 0 possitiveNumber
+    new BigInteger (value)
+
+let BigOne = new BigInteger(1)
+
+let checkRange primeP value =
+    value > BigOne && value < primeP
+
+let rec generate primeP =
+    let check = checkRange primeP
+    let value = getRandom primeP
+
+    match check value with
+    | true -> value
+    | false -> generate primeP
+
+let privateKey primeP = 
+    generate primeP
+
+let publicKey (primeP :BigInteger) (primeG :BigInteger) (privateKey :BigInteger) = 
+    BigInteger.ModPow(primeG,privateKey,primeP)
+    
+
+let secret primeP publicKey privateKey = 
+    BigInteger.ModPow(publicKey,privateKey,primeP)
